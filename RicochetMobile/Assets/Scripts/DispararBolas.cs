c207241c8d5ac7b7fa5106790e6c4ba301088043
Pedrito16 <pedrogabrielvicente98@gmail.com>
2025-03-27 16:38:49 -0300

    eu fiz as bolinha quicar B)

--- RicochetMobile/Assets/Scripts/DispararBolas.cs
+++ RicochetMobile/Assets/Scripts/DispararBolas.cs
@@ -4,20 +4,53 @@ using UnityEngine;
 
 public class DispararBolas : MonoBehaviour
 {
+    [SerializeField] int quantidadeBolasMax;
     [SerializeField] GameObject bolinha;
+    [Header("Configurações")]
+    [SerializeField] float velocidadeBolas;
+    
+    [Header("Debug")]
+    [SerializeField] List<Rigidbody2D> ballsRbList;
+
+    // variaveis fora do console
+    private void Awake()
+    {
+
+    }
     void Start()
     {
-        
+        //pool para bolinhas
+        for(int i = 0; i < quantidadeBolasMax; i++)
+        {
+            GameObject ball = Instantiate(bolinha);
+            ball.transform.SetParent(transform, false);
+            ball.transform.position = transform.position;
+            ball.SetActive(false);
+            ballsRbList.Add(ball.GetComponent<Rigidbody2D>());
+        }
     }
 
-    // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (Input.touchCount > 0 && Input.GetMouseButtonDown(0))
         {
-            Vector2 mousePos = Camera.main.WorldToScreenPoint(Input.mousePosition);
-            print(mousePos);
-            GameObject bola = Instantiate(bolinha, mousePos, bolinha.transform.rotation);
-        }   
+            Touch touch = Input.GetTouch(0);
+            Vector3 mousePos = Camera.main.ScreenToWorldPoint(touch.position);
+            print("atirando bolas na posição: " + mousePos);
+            StartCoroutine(ShotBalls(mousePos));
+        }
+        
+    }
+    IEnumerator ShotBalls(Vector3 mousePos)
+    {
+        print("atirando bolas");
+        for(int i = 0; i < ballsRbList.Count; i++)
+        {
+            /*Vector2 position = ballsRbList[i].transform.position  mousePos;
+            position = position.normalized;*/
+            ballsRbList[i].gameObject.SetActive(true);
+            ballsRbList[i].velocity = position * velocidadeBolas;
+            yield return new WaitForSeconds(0.1f);
+        }
     }
 }

768ce60dd39a7316cd95bd63b7e2b35ffbf95130
Pedrito16 <proibidofantasma@gmail.com>
2025-08-29 17:04:12 -0300

    arrumei o loader inteiro e ta bem legal

--- RicochetMobile/Assets/Scripts/CenarioLoader.cs
+++ RicochetMobile/Assets/Scripts/CenarioLoader.cs
@@ -13,46 +13,52 @@ public class CenarioLoader : MonoBehaviour
     [Header("Cenario Elements - single")]
     [SerializeField] Transform topDecoration;
     [SerializeField] float growSize;
+
+    [SerializeField] CenarioSO cenario;
     void Start()
     {
-        StartCoroutine(GrowShrinkEffect(sideDecorations));
-        StartCoroutine(GrowShrinkEffect(barriers));
+        LoadCenario(cenario);
     }
 
     void Update()
     {
         
     }
-    public void LoadCenario(CenarioSO cenario)
+    public void LoadCenario(CenarioSO scenario)
     {
-        StartCoroutine(GrowShrinkEffect(sideDecorations, cenario.sideDecorationsSprites));
+        StartCoroutine(GrowShrinkEffect(sideDecorations, scenario.sideDecorationsSprites));
+        StartCoroutine(SwitchSpriteNoEffect(sideWalls, new Sprite[] { scenario.sideWalls }));
+         
+        StartCoroutine(GrowShrinkEffect(new Transform[] {topDecoration}, new Sprite[] {scenario.topSprite}));
 
-        StartCoroutine(GrowShrinkEffect(barriers, new Sprite[] { cenario.barrierSprite })); //convertendo para array
-        StartCoroutine(GrowShrinkEffect(new Transform[] {topDecoration}, new Sprite[] {cenario.topSprite}));
-
-        StartCoroutine(SwitchSpriteNoEffect(sideWalls, cenario.sideWalls));
-        StartCoroutine(SwitchSpriteNoEffect(grounds, cenario.groundSprite));
+        StartCoroutine(SwitchSpriteNoEffect(grounds, new Sprite[] { scenario.groundSprite }));
+        StartCoroutine(GrowShrinkEffect(barriers, new Sprite[] { scenario.barrierSprite })); //convertendo para array        
     }
     IEnumerator GrowShrinkEffect(Transform[] array, Sprite[] spritesToChange = null)
     {
         bool isSpritesNull = spritesToChange == null;
+        bool hasOnlyOneSprite = spritesToChange?.Length == 1;
+
         for (int i = 0; i < array.Length; i++)
         {
+            
             Transform objectToChange = array[i];
-            objectToChange.gameObject.SetActive(true);
             Vector3 vector3 = objectToChange.localScale;
+
+            objectToChange.gameObject.SetActive(true);
             objectToChange.DOScale(new Vector3(vector3.x + growSize, vector3.y + growSize, vector3.z + growSize), 0.5f).SetLoops(2, LoopType.Yoyo).SetEase(Ease.InOutSine);
+
             if(!isSpritesNull)
             {
-                Sprite spriteToChange = spritesToChange[i];
-                SpriteRenderer sideDecorationSR = objectToChange.GetComponent<SpriteRenderer>();
-                if (spriteToChange != null)
+                SpriteRenderer objectToChangeRenderer = objectToChange.GetComponent<SpriteRenderer>();
+
+                if (hasOnlyOneSprite)
                 {
-                    sideDecorationSR.sprite = spriteToChange;
+                    objectToChangeRenderer.sprite = spritesToChange[0];
                 }
                 else
                 {
-                    objectToChange.gameObject.SetActive(false);
+                    objectToChangeRenderer.sprite = spritesToChange[i];
                 }
             }
             yield return new WaitForSeconds(0.2f);
@@ -60,16 +66,24 @@ public class CenarioLoader : MonoBehaviour
     }
     IEnumerator SwitchSpriteNoEffect(Transform[] array, params Sprite[] newSprites)
     {
+        bool hasOnlyOneSprite = newSprites.Length == 1;
+        Sprite loneSprite = hasOnlyOneSprite ? newSprites[0] : null;
         for(int i = 0; i < array.Length; i++)
         {
-            SpriteRenderer sideDecoration = sideDecorations[i].GetComponent<SpriteRenderer>();
+            SpriteRenderer sideDecoration = array[i].GetComponent<SpriteRenderer>();
             Sprite sprite = sideDecoration.sprite;
-
-            if(sprite == null) sideDecoration.gameObject.SetActive(false);
-            else sideDecoration.sprite = sprite;
-
-            yield return new WaitForSeconds(0.2f);
+            print("trocando sprite para: " + sprite.name);
+            if (sprite == null) 
+            {
+                sideDecoration.gameObject.SetActive(false);
+            }
+            else
+            {
+                if (hasOnlyOneSprite)
+                    sideDecoration.sprite = loneSprite;
+                else sideDecoration.sprite = sprite;
+            }
+            yield return new WaitForSeconds(0.3f);
         }
     }
-    
 }
